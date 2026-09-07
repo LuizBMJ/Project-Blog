1671426565abe00d691be88afa8b718ad4552f9e
Jorge Luiz Bevilacqua Mercer <luiz.bevilacquam@gmail.com>
2026-09-07 17:36:27 -0400

    Feat(Container): Add Container component and update home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 import { Suspense } from 'react'
 import { PostsList } from '../components/PostList'
 import { SpinLoader } from '../components/SpinLoader'
+import { Container } from '../components/Container'
 
 export default function HomePage() {
   return (
-    <div>
+    <Container>
       <header>
         <h1 className="text-6xl font-bold text-center py-8">Aqui é a HEADER</h1>
       </header>
+
+      <p className="text-justify">
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+      </p>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
@@ -16,6 +24,6 @@ export default function HomePage() {
       <footer>
         <p className="text-6xl font-bold text-center py-8">Footer</p>
       </footer>
-    </div>
+    </Container>
   )
 }
