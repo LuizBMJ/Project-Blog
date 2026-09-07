e2646abfbe23a92413f000db2550bfc7dedd77fd
Jorge Luiz Bevilacqua Mercer <luiz.bevilacquam@gmail.com>
2026-09-07 17:19:10 -0400

    Feat(PostList): Add PostList component and refactor home page layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,24 @@
-import clsx from 'clsx'
-import { SpinLoader } from '../SpinLoader'
+'use client'
 
-export default function HomePage() {
+import clsx from 'clsx'
+
+export function Header() {
+  console.log('HEADER')
+
   return (
-    <div>
-      <SpinLoader containerClasses={clsx('min-h-[500px]', 'bg-amber-500')} />
-    </div>
+    <h1
+      className={clsx(
+        'text-6xl',
+        'font-bold',
+        'text-blue-500',
+        'hover:text-blue-50',
+        'hover:bg-blue-500',
+        'transition',
+        'duration-1000',
+      )}
+      onClick={() => alert(123)}
+    >
+      Texto no meu h1
+    </h1>
   )
 }
